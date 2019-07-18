b093115e708b97308a6bb32f869c59792137b359
alekeyi <aleksey.ioseliani@gmail.com>
2019-07-18 17:32:02 -0500

    Compresses bundle for optimization

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 3003;
+const compression = require('compression');
+
+app.use(compression());
 
 function ignoreFavicon(req, res, next) {
   if (req.originalUrl === '/favicon.ico') {
