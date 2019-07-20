c85c6f1945adf643fb5fbf621cf1930d18980fe9
alekeyi <aleksey.ioseliani@gmail.com>
2019-07-19 19:45:57 -0500

    Removed unnecessary JQuery, updated schema

--- schema.sql
+++ schema.sql
@@ -4,15 +4,12 @@ CREATE DATABASE regretsy_images;
 
 USE regretsy_images;
 
-CREATE TABLE listings (
-    id SERIAL,
-    listing_id BIGINT NOT NULL,
-    PRIMARY KEY (ID)
-);
-
 CREATE TABLE images (
     id SERIAL,
-    listing_id BIGINT NOT NULL,
-    mainImage BOOLEAN,
-    PRIMARY KEY (ID),
+    listing_id INT NOT NULL,
+    75x VARCHAR(20),
+    170x VARCHAR(20),
+    570x VARCHAR(20),
+    fullx VARCHAR(20),
+    PRIMARY KEY (ID)
 );
